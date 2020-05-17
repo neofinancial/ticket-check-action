0b71ef04c9e8f28ff7bb37b16cbb4a144fbeec1d
Austin Paquette <austin.paquette@neofinancial.com>
2020-05-17 17:34:37 -0600

    fix incorrect variable reference

--- src/index.ts
+++ src/index.ts
@@ -44,14 +44,13 @@ async function run(): Promise<void> {
     const titleFormat = getInput('titleFormat', { required: true });
 
     // Check for a ticket reference in the branch
-
     const branch: string = context.payload.pull_request?.head.ref;
     const branchRegexBase = getInput('branchRegex', { required: true });
     const branchRegexFlags = getInput('branchRegexFlags', {
       required: true
     });
     const branchRegex = new RegExp(branchRegexBase, branchRegexFlags);
-    const branchCheck = title.match(branchRegex);
+    const branchCheck = branch.match(branchRegex);
 
     if (branchCheck !== null) {
       debug('Branch name contains a reference to a ticket, updating title');
