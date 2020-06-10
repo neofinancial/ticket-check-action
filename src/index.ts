716533c5f5f99869a3ff191081015d9abe4b7f69
Austin Paquette <austin.paquette@neofinancial.com>
2020-06-09 20:47:56 -0600

    fix typo

--- src/index.ts
+++ src/index.ts
@@ -44,9 +44,9 @@ async function run(): Promise<void> {
     const exemptUsers = getInput('exemptUsers', { required: true }).split(',');
 
     if (sender && exemptUsers.includes(sender.login)) {
-      debug('Username is listing as exempt');
-      debug(exemptUsers.join(', '));
-      debug(sender?.login);
+      debug('Username is listed as exempt');
+      debug(`Exempt Users: ${exemptUsers.join(', ')}`);
+      debug(`Pull Request Owner: ${sender.login}`);
       return;
     }
 
