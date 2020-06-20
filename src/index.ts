7503e051988e78f34d3d644bd82d869ee6531dae
Austin Paquette <austin.paquette@neofinancial.com>
2020-06-20 16:06:27 -0600

    fix incorrect boolean value

--- src/index.ts
+++ src/index.ts
@@ -41,7 +41,7 @@ async function run(): Promise<void> {
     const { sender } = context.payload;
 
     // Exempt Users
-    const exemptUsers = getInput('exemptUsers', { required: true })
+    const exemptUsers = getInput('exemptUsers', { required: false })
       .split(',')
       .map(user => user.trim());
 
