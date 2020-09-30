09a8607cfbc9a90ac51d81c5b91b6a45ab1ae2c3
Ian Sutherland <ian@iansutherland.ca>
2020-09-30 13:35:29 -0600

    Revert everything

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,7 @@
 /* eslint-disable @typescript-eslint/camelcase */
 
 import { debug as log, getInput, setFailed } from '@actions/core';
-import { context } from '@actions/github';
-import { Octokit } from '@octokit/action';
+import { context, GitHub } from '@actions/github';
 
 // Helper function to retrieve ticket number from a string (either a shorthand reference or a full URL)
 const extractId = (value: string): string | null => {
@@ -46,7 +45,8 @@ async function run(): Promise<void> {
     }
 
     // Instantiate a GitHub Client instance
-    const client = new Octokit();
+    const token = getInput('token', { required: true });
+    const client = new GitHub(token);
     const { owner, repo, number } = context.issue;
     const login = context.payload.pull_request?.user.login as string;
     const senderType = context.payload.pull_request?.user.type as string;
